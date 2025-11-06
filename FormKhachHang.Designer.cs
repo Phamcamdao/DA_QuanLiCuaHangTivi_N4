d638d73c90cbe0c070744a3df550ba9cc0d9ac97
DTH235620-DinhThanhDanh <danh_dth235620@student.agu.edu.vn>
2025-11-07 00:15:58 +0700

    them

---
+++ FormKhachHang.Designer.cs
@@ -0,0 +1,318 @@
+﻿namespace DA_QuanLiCuaHangTivi
+{
+    partial class FormKhachHang
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormKhachHang));
+            label8 = new Label();
+            menuXoatrang = new ToolStripMenuItem();
+            xoáToolStripMenuItem = new ToolStripMenuItem();
+            menuSua = new ToolStripMenuItem();
+            memuThem = new ToolStripMenuItem();
+            txtSearch = new TextBox();
+            btnTim = new Button();
+            dataGridView1 = new DataGridView();
+            menuThoat = new ToolStripMenuItem();
+            rdaNu = new RadioButton();
+            rdaNam = new RadioButton();
+            textBox6 = new TextBox();
+            label7 = new Label();
+            label5 = new Label();
+            textBox4 = new TextBox();
+            label3 = new Label();
+            txtTenkh = new TextBox();
+            label2 = new Label();
+            label4 = new Label();
+            txtMakh = new TextBox();
+            label1 = new Label();
+            menuStrip1 = new MenuStrip();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Times New Roman", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 163);
+            label8.Location = new Point(348, 36);
+            label8.Name = "label8";
+            label8.Size = new Size(348, 38);
+            label8.TabIndex = 27;
+            label8.Text = "Danh mục khách hàng";
+            // 
+            // menuXoatrang
+            // 
+            menuXoatrang.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 163);
+            menuXoatrang.Image = (Image)resources.GetObject("menuXoatrang.Image");
+            menuXoatrang.Name = "menuXoatrang";
+            menuXoatrang.Size = new Size(121, 26);
+            menuXoatrang.Text = "Xóa trắng";
+            // 
+            // xoáToolStripMenuItem
+            // 
+            xoáToolStripMenuItem.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 163);
+            xoáToolStripMenuItem.Image = (Image)resources.GetObject("xoáToolStripMenuItem.Image");
+            xoáToolStripMenuItem.Name = "xoáToolStripMenuItem";
+            xoáToolStripMenuItem.Size = new Size(77, 26);
+            xoáToolStripMenuItem.Tag = "menuXoa";
+            xoáToolStripMenuItem.Text = "Xoá";
+            // 
+            // menuSua
+            // 
+            menuSua.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 163);
+            menuSua.Image = (Image)resources.GetObject("menuSua.Image");
+            menuSua.Name = "menuSua";
+            menuSua.Size = new Size(75, 26);
+            menuSua.Text = "Sửa";
+            // 
+            // memuThem
+            // 
+            memuThem.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 163);
+            memuThem.Image = (Image)resources.GetObject("memuThem.Image");
+            memuThem.Name = "memuThem";
+            memuThem.Size = new Size(88, 26);
+            memuThem.Text = "Thêm";
+            // 
+            // txtSearch
+            // 
+            txtSearch.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 163);
+            txtSearch.Location = new Point(757, 594);
+            txtSearch.Name = "txtSearch";
+            txtSearch.Size = new Size(169, 30);
+            txtSearch.TabIndex = 25;
+            // 
+            // btnTim
+            // 
+            btnTim.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 163);
+            btnTim.Location = new Point(935, 586);
+            btnTim.Name = "btnTim";
+            btnTim.Size = new Size(94, 44);
+            btnTim.TabIndex = 24;
+            btnTim.Text = "Tìm kiếm";
+            btnTim.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(29, 225);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(1000, 355);
+            dataGridView1.TabIndex = 22;
+            // 
+            // menuThoat
+            // 
+            menuThoat.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 163);
+            menuThoat.Image = (Image)resources.GetObject("menuThoat.Image");
+            menuThoat.Name = "menuThoat";
+            menuThoat.Size = new Size(89, 26);
+            menuThoat.Text = "Thoát";
+            // 
+            // rdaNu
+            // 
+            rdaNu.AutoSize = true;
+            rdaNu.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 163);
+            rdaNu.Location = new Point(842, 86);
+            rdaNu.Name = "rdaNu";
+            rdaNu.Size = new Size(56, 26);
+            rdaNu.TabIndex = 20;
+            rdaNu.TabStop = true;
+            rdaNu.Text = "Nữ";
+            rdaNu.UseVisualStyleBackColor = true;
+            // 
+            // rdaNam
+            // 
+            rdaNam.AutoSize = true;
+            rdaNam.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 163);
+            rdaNam.Location = new Point(706, 86);
+            rdaNam.Name = "rdaNam";
+            rdaNam.Size = new Size(68, 26);
+            rdaNam.TabIndex = 21;
+            rdaNam.TabStop = true;
+            rdaNam.Text = "Nam";
+            rdaNam.UseVisualStyleBackColor = true;
+            // 
+            // textBox6
+            // 
+            textBox6.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 163);
+            textBox6.Location = new Point(706, 129);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(250, 30);
+            textBox6.TabIndex = 16;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 163);
+            label7.Location = new Point(600, 597);
+            label7.Name = "label7";
+            label7.Size = new Size(151, 22);
+            label7.TabIndex = 9;
+            label7.Text = "Tìm kiếm theo tên";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 163);
+            label5.Location = new Point(565, 134);
+            label5.Name = "label5";
+            label5.Size = new Size(114, 22);
+            label5.TabIndex = 12;
+            label5.Text = "Số điện thoại";
+            // 
+            // textBox4
+            // 
+            textBox4.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 163);
+            textBox4.Location = new Point(170, 179);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(786, 30);
+            textBox4.TabIndex = 17;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 163);
+            label3.Location = new Point(29, 179);
+            label3.Name = "label3";
+            label3.Size = new Size(68, 22);
+            label3.TabIndex = 11;
+            label3.Text = "Địa chỉ";
+            // 
+            // txtTenkh
+            // 
+            txtTenkh.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 163);
+            txtTenkh.Location = new Point(170, 134);
+            txtTenkh.Name = "txtTenkh";
+            txtTenkh.Size = new Size(234, 30);
+            txtTenkh.TabIndex = 18;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 163);
+            label2.Location = new Point(565, 90);
+            label2.Name = "label2";
+            label2.Size = new Size(81, 22);
+            label2.TabIndex = 10;
+            label2.Text = "Giới tính";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 163);
+            label4.Location = new Point(25, 142);
+            label4.Name = "label4";
+            label4.Size = new Size(131, 22);
+            label4.TabIndex = 14;
+            label4.Text = "Tên khách hàng";
+            // 
+            // txtMakh
+            // 
+            txtMakh.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 163);
+            txtMakh.Location = new Point(170, 90);
+            txtMakh.Name = "txtMakh";
+            txtMakh.Size = new Size(234, 30);
+            txtMakh.TabIndex = 19;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 163);
+            label1.Location = new Point(29, 98);
+            label1.Name = "label1";
+            label1.Size = new Size(127, 22);
+            label1.TabIndex = 13;
+            label1.Text = "Mã khách hàng";
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { memuThem, menuSua, xoáToolStripMenuItem, menuXoatrang, menuThoat });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(1052, 30);
+            menuStrip1.TabIndex = 26;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // FormKhachHang
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1052, 636);
+            Controls.Add(label8);
+            Controls.Add(txtSearch);
+            Controls.Add(btnTim);
+            Controls.Add(dataGridView1);
+            Controls.Add(rdaNu);
+            Controls.Add(rdaNam);
+            Controls.Add(textBox6);
+            Controls.Add(label7);
+            Controls.Add(label5);
+            Controls.Add(textBox4);
+            Controls.Add(label3);
+            Controls.Add(txtTenkh);
+            Controls.Add(label2);
+            Controls.Add(label4);
+            Controls.Add(txtMakh);
+            Controls.Add(label1);
+            Controls.Add(menuStrip1);
+            Name = "FormKhachHang";
+            Text = "FormKhachHang";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label8;
+        private ToolStripMenuItem menuXoatrang;
+        private ToolStripMenuItem xoáToolStripMenuItem;
+        private ToolStripMenuItem menuSua;
+        private ToolStripMenuItem memuThem;
+        private TextBox txtSearch;
+        private Button btnTim;
+        private DataGridView dataGridView1;
+        private ToolStripMenuItem menuThoat;
+        private RadioButton rdaNu;
+        private RadioButton rdaNam;
+        private TextBox textBox6;
+        private Label label7;
+        private Label label5;
+        private TextBox textBox4;
+        private Label label3;
+        private TextBox txtTenkh;
+        private Label label2;
+        private Label label4;
+        private TextBox txtMakh;
+        private Label label1;
+        private MenuStrip menuStrip1;
+    }
+}
